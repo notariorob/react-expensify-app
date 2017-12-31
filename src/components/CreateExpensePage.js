54cc9b18ff8e27a22c1dce25071d36467ddf824b
Roberto Notario <notariorob@gmail.com>
2017-12-31 22:50:46 +0000

    Move components and router to their own file

---
+++ src/components/CreateExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+);
+
+export default CreateExpensePage;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import CreateExpensePage from '../components/CreateExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import Header from '../components/Header';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact/>
+        <Route path="/create" component={CreateExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
