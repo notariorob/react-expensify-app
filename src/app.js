0f0e9ae3122bd32a32b359519423a96f8205e0a2
Roberto Notario <notariorob@gmail.com>
2018-01-05 12:44:07 +0000

    Add date and state to form and set submit prop

--- src/app.js
+++ src/app.js
@@ -13,11 +13,11 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
+// store.subscribe(() => {
+//   const state = store.getState();
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//   console.log(visibleExpenses);
+// });
 
 store.dispatch(addExpense({
   description: 'Water bill',
