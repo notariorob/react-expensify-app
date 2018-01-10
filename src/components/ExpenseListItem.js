8135032f15cb16eca60f5562916ef68f005e55c1
Roberto Notario <notariorob@gmail.com>
2018-01-10 10:19:52 +0000

    use numeraljs for currency formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 import { editExpense } from '../actions/expenses';
 
@@ -9,8 +10,8 @@ import { editExpense } from '../actions/expenses';
     <Link to={"/edit/" + id}>
       <h2>{description}</h2>
     </Link>
-    <p>Amount: {amount}</p>
-    <p>Created at: {moment(createdAt).format('MMM Do, YYYY')}</p>
+    <p>{numeral(amount / 100).format('$0,00.00')} -
+    Created at: {moment(createdAt).format('MMMM Do, YYYY')}</p>
   </div>
 );
 
